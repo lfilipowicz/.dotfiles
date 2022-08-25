e65d445322074fd4bc72783e3c81789706060372
Łukasz Filipowicz <filipowicz.lukasz@gmail.com>
2022-08-25 19:03:47 +0200

    add focus in nvim tree

--- nvim/lua/bizon/nvim-tree.lua
+++ nvim/lua/bizon/nvim-tree.lua
@@ -4,7 +4,18 @@ if not status_ok then
 end
 
 plugin.setup({
+  update_focused_file = {
+    -- enables the feature
+    enable = true,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd = false,
+    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+    ignore_list = {},
+  },
   view = {
+
     adaptive_size = true,
     centralize_selection = false,
     hide_root_folder = false,
