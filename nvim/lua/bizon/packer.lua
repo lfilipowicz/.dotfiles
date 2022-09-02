0a60fb067ee6d26dd21dae1d605200c3e26a3937
Łukasz Filipowicz <filipowicz.lukasz@gmail.com>
2022-09-02 07:49:54 +0200

    add treesitter context

--- nvim/lua/bizon/packer.lua
+++ nvim/lua/bizon/packer.lua
@@ -39,6 +39,7 @@ return require("packer").startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   })
+  use("nvim-treesitter/nvim-treesitter-context")
 
   --     -- TELESCOPE
   use("nvim-lua/popup.nvim")
