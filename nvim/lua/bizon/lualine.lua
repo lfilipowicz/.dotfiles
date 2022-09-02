52765b239784e2ed43ff0e90b78573ff87d36485
Łukasz Filipowicz <filipowicz.lukasz@gmail.com>
2022-09-02 07:50:29 +0200

    disable lualina for some apps

--- nvim/lua/bizon/lualine.lua
+++ nvim/lua/bizon/lualine.lua
@@ -9,10 +9,7 @@ plugin.setup({
     theme = "auto",
     component_separators = { left = "", right = "" },
     section_separators = { left = "", right = "" },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
+    disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
     ignore_focus = {},
     always_divide_middle = true,
     globalstatus = false,
