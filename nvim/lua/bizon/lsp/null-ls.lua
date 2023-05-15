3694fe4e87899a2a21541e5a6865cb85e0f75f0a
Łukasz Filipowicz <filipowicz.lukasz@gmail.com>
2023-05-15 10:16:17 +0200

    chore(nvim): move rust config

--- nvim/lua/bizon/lsp/null-ls.lua
+++ nvim/lua/bizon/lsp/null-ls.lua
@@ -23,24 +23,8 @@ null_ls.setup({
     formatting.prettierd, -- js/ts formatter
     formatting.stylua, -- lua formatter
     diagnostics.eslint_d,
-    formatting.rustfmt.with({
-      extra_args = function(params)
-        local Path = require("plenary.path")
-        local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
-
-        if cargo_toml:exists() and cargo_toml:is_file() then
-          for _, line in ipairs(cargo_toml:readlines()) do
-            local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
-            if edition then
-              return { "--edition=" .. edition }
-            end
-          end
-        end
-        -- default edition when we don't find `Cargo.toml` or the `edition` in it.
-        return { "--edition=2021" }
-      end,
-    }),
     diagnostics.tsc,
+    -- formatting.sqlfmt,
     formatting.shfmt,
     code_action.shellcheck,
     code_action.eslint,
